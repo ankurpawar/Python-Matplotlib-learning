0039c9f316ccc314f88593fe509e0af67e42208d
ankurpawar <ankurpawar@users.noreply.github.com>
2022-04-03 17:03:13 +0530

    Save animation to gif, keep axis square

--- Animation/CircleRadiusAnim.py
+++ Animation/CircleRadiusAnim.py
@@ -3,20 +3,26 @@ import matplotlib.pyplot as plt
 from numpy import pi
 from matplotlib.lines import Line2D
 import matplotlib.animation as animation
+from matplotlib.animation import PillowWriter
 
-theta = np.linspace(0, 2*pi, 500);
+theta = np.linspace(0, 2 * pi, 500);
 r = np.ones(500)
 x = r * np.cos(theta)
 y = r * np.sin(theta)
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
+fig, ax = plt.subplots()
+plt.plot()
 
 line = Line2D(x, y)
 ax.add_line(line)
 
 radius = Line2D([0, 0], [0, 1])
+
 ax.add_line(radius)
+ax.axis('equal')
+ax.set_aspect('equal', 'box')
+ax.set_xlim(min(x + x * 0.1), max(x + x * 0.1))
+ax.set_ylim(min(y + y * 0.1), max(y + y * 0.1))
 
 def animate(i):
     radius.set_data([0, x[i]],[0, y[i]])
@@ -26,8 +32,6 @@ def animate(i):
 myAnimation = animation.FuncAnimation(fig, animate, frames=np.arange(0, 500, 20), \
                                       interval=20, blit=True, repeat=True)
 
-ax.set_xlim(min(x), max(x))
-ax.set_ylim(min(y), max(y))
+myAnimation.save("circleanim.gif", writer=PillowWriter(fps=24))
 
-plt.axis('equal')
 plt.show()
